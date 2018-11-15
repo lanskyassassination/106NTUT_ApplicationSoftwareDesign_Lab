17e44fabe8a534f0cfa713b531d884cdb73c2ce6
lanskyassassination <lanskyassassination@gmail.com>
2018-11-15 15:04:08 +0800

    add lab8..

--- Lab8/app/src/main/java/com/example/user/lab8/Myservice.java
+++ Lab8/app/src/main/java/com/example/user/lab8/Myservice.java
@@ -1,4 +1,37 @@
 package com.example.user.lab8;
 
-public class Myservice {
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MyService extends Service {
+    public MyService() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    Thread.sleep(5000);
+
+                    Intent intent = new Intent(MyService.this,Main2Activity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    MyService.this.startActivity(intent);
+
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+        return  START_STICKY;
+    }
 }
